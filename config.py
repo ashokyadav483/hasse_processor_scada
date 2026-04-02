9ccbe5aa45d9dce6ad59bf25783189abdcba8dfb
ashokyadav483 <ashok.yadav483@gmail.com>
2026-04-02 17:47:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+PLC_URL = "opc.tcp://192.168.0.2:4840"
+NODE_PREHEAT_FAN_TEMP= "ns=4;i=2"
+NODE_PREHEAT_LEFT_TEMP = "ns=4;i=3"
+
+MONGO_URL = "mongodb://localhost:27017/"
+DB_NAME = "plc_scada"
+COLLECTION = "live_data"
